71fe60351b64c3aad21db18d4da84b0d01a89a89
Santi <profsantirp@gmail.com>
2022-03-12 12:37:12 +0100

    ejecution await and handle error

--- 02-fundamentals-nodejs/async_await.js
+++ 02-fundamentals-nodejs/async_await.js
@@ -1,47 +1,75 @@
-const empleados = [
+const employees = [
     {
         id: 1,
-        nombre: 'Fernando'
+        name: 'Fernando'
     },
     {
         id: 2,
-        nombre: 'Linda'
+        name: 'Linda'
     },
     {
         id: 3,
-        nombre: 'Karen'
+        name: 'Karen'
     }
 ];
 
-const salarios = [
+const salaries = [
     {
         id: 1,
-        salario: 1000
+        salary: 1000
     },
     {
         id: 2,
-        salario: 1500
+        salary: 1500
     },
     {
         id: 3,
     }
 ];
 
-const getEmpleado = ( id ) => {
-    return new Promise(( resolve, reject ) =>{
-        
-        const empleado = empleados.find( e => e.id === id )?.nombre;
-         ( empleado )
-             ? resolve( empleado )
-              : reject( `No existe empleado con id ${ id }`);
+const getEmployee = (id) => {
+    return new Promise((resolve, reject) => {
+
+        const employee = employees.find(e => e.id === id)?.name;
+        (employee)
+            ? resolve(employee)
+            : reject(`No employee exists with id ${id}`);
     });
 }
 
-const getSalario = () => {
-    return new Promise(( resolve, reject ) => {
-         const salario = salarios.find( s => s.id === id )?.salario; 
-          ( salario )
-              ? resolve( salario )
-               : reject( `No existe salario con id ${ id }` );
-     });
+const getSalary = (id) => {
+    return new Promise((resolve, reject) => {
+        const salary = salaries.find(s => s.id === id)?.salary;
+        (salary)
+            ? resolve(salary)
+            : reject(`No salary exists with id ${id}`);
+    });
 }
+
+// async transforma una function a una promesa que se recoge con .then
+// el await solo se puede utiliza dentro de funciones async 
+
+/**
+ * ! ERRORS 
+ *  UnhandledPromiseRejectionWarning: Unhandled promise rejection, 
+ *      se da cuando no se gestiona un error de una promesa o funcion async
+ * 
+ * */
+
+getInfoUser = async (id) => {
+    try {
+        const employee = await getEmployee(id);
+        const salary = await getSalary(id);
+
+        return `The salary from employee: ${employee} is ${salary}`
+    } catch (error) {
+        return error;   // * correct execution and go by .then, we handle exception
+        //throw error;    // * incorrect execution and go by .catch  we dont handle exception
+    }
+}
+
+const idEmployee = 3;
+
+getInfoUser(idEmployee)
+    .then(msg => console.log(msg))
+    .catch(error => console.log(error));
