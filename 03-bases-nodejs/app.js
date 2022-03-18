77450c34325023b964182e21aee0b817cfaedf6a
SantDani <47375284+SantDani@users.noreply.github.com>
2022-03-18 23:09:15 +0100

    get params from console

--- 03-bases-nodejs/app.js
+++ 03-bases-nodejs/app.js
@@ -1,7 +1,11 @@
 
 const { createTableInFile } = require('./helpers/multiply')
 
-const tableNumber = 7;
 
-createTableInFile(tableNumber).
-    then(result => console.log(result))
+
+const [, , arg3] = process.argv;
+const [, tableNumber = 1] = arg3.split('=');
+console.log("🚀 ~ file: app.js ~ line 9 ~ arg3", tableNumber);
+
+createTableInFile(tableNumber)
+    .then(result => console.log(result))
