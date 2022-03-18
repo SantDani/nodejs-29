55909ba6cb9a1bd6a2680b0075540466a9d6fa1c
SantDani <47375284+SantDani@users.noreply.github.com>
2022-03-18 22:59:38 +0100

    refactor to file and handle error

--- 03-bases-nodejs/app.js
+++ 03-bases-nodejs/app.js
@@ -1,23 +1,7 @@
 
-const fileSystem = require('fs') // or in new versions:  import { write, writeFile } from 'fs'
+const { createTableInFile } = require('./helpers/multiply')
 
-const tableNumber = 9;
-let output = "";
-console.clear();
-console.log("========================")
-console.log("Table ", tableNumber);
-console.log("========================")
+const tableNumber = 7;
 
-
-for (let i = 0; i < 11; i++) {
-    output += (`5 x ${i} = ${tableNumber * i} \n`);
-}
-
-console.log(output)
-const nameFile = `table${tableNumber}.txt`;
-fileSystem.writeFile(nameFile, output, (err) => {
-    if (err) throw err;
-    console.log(`The file: ${nameFile} has been saved`);
-})
-
-
+createTableInFile(tableNumber).
+    then(result => console.log(result))
---
+++ 03-bases-nodejs/helpers/multiply.js
@@ -0,0 +1,32 @@
+const fileSystem = require('fs') // or in new versions:  import { write, writeFile } from 'fs'
+
+
+const createTableInFile = async (tableNumber = 1) => {
+    let output = "";
+    console.clear();
+    console.log("========================")
+    console.log("Table ", tableNumber);
+    console.log("========================")
+
+
+    for (let i = 0; i < 11; i++) {
+        output += (`5 x ${i} = ${tableNumber * i} \n`);
+    }
+
+    console.log(output)
+    const nameFile = `table${tableNumber}.txt`;
+
+
+    try {
+        fileSystem.writeFile(nameFile, output, (err) => {
+            if (err) throw err;
+        })
+        return (`The file: ${nameFile} has been saved`);
+    } catch (error) {
+        return error;
+    }
+}
+
+module.exports = {
+    createTableInFile // or  createTableInFile: createTableInFile
+}
