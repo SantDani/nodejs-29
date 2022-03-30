576dd3e1aa6be7af998088516e8e4dc43ce9d0b5
SantDani <47375284+SantDani@users.noreply.github.com>
2022-03-30 18:35:42 +0200

    fix pause

--- 04-to-do/helpers/inquirer.js
+++ 04-to-do/helpers/inquirer.js
@@ -8,27 +8,27 @@ const menuQuestions = [
     message: "Chose a option",
     choices: [
       {
-        value: 1,
+        value: '1',
         name: "1. New task",
       },
       {
-        value: 2,
-        name: "2. List completed task`",
+        value: '2',
+        name: "2. List completed task",
       },
       {
-        value: 3,
+        value: '3',
         name: "3. List pending task",
       },
       {
-        value: 4,
+        value: '4',
         name: "4. Complete task(s)",
       },
       {
-        value: 5,
+        value: '5',
         name: "5. Delete task",
       },
       {
-        value: 0,
+        value: '0',
         name: "0. Exit",
       },
     ],
@@ -36,7 +36,7 @@ const menuQuestions = [
 ];
 
 const menuInquirer = async () => {
-  //   console.clear();
+    console.clear();
   console.log("=================================".green);
   console.log("Chose a option");
   console.log("=================================\n".green);
@@ -47,19 +47,22 @@ const menuInquirer = async () => {
 };
 
 const pause = async () => {
-  return new Promise((reject) => {
-    const question = {
+
+  const question = [
+    {
       type: "input",
       name: "someKey",
       message: `Write ${"some key".green} to continue\n`,
-    };
+    }
+  ];
 
-    inquirer.prompt(question);
-    000001234;
-  });
+  await inquirer.prompt(question);
 };
+
+
 
 module.exports = {
   menuInquirer,
   pause,
+  readInput
 };
