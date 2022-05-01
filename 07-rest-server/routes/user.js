8d6b069b421c1fcb795a11c946cc442b5653672b
SantDani <47375284+SantDani@users.noreply.github.com>
2022-05-01 17:43:43 +0200

    validate role from database

--- 07-rest-server/routes/user.js
+++ 07-rest-server/routes/user.js
@@ -1,6 +1,7 @@
 const {Router} = require('express');
 const { check } = require('express-validator');
 const { fieldValidator } = require('../middlewares/validate-fields');
+const Role = require('../models/role');
 const { userGet, userPost, userDelete, userPatch, userPut,  } = require('./../controllers/user')
 
 
@@ -11,7 +12,14 @@ router.post('/',
 [ 
     check('email', 'Email not valid').isEmail(),
     check('name', 'Name can not be empty').not().isEmpty(),
-    check('rol', 'Not is a valid rol').isIn(['ADMIN_ROLE', 'USER_ROLE']),
+    // check('rol', 'Not is a valid rol').isIn(['ADMIN_ROLE', 'USER_ROLE']),
+    check('rol', 'Not is a valid rol').custom( async (rol = '' )=>{
+         const existsRol = await Role.findOne({rol})
+
+        if(!existsRol){
+            throw new Error( `This role  ${rol} not exists`);
+        }
+    }),
     check('password', 'Password: minimum 6 characters').isLength({min: 6}),
     fieldValidator
 ]
